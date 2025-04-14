3dd8088af48f785ffcce8ef117760d2e5c1a8617
Mihael Baranašić <mbaranasi21@foi.hr>
2025-04-14 10:26:50 +0200

    Vježba 5

---
+++ mbaranasi21_vjezba_05/mbaranasi21_vjezba_05_lib_dnevnik/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module mbaranasi21_vjezba_05_lib_dnevnik {
+  exports edu.unizg.foi.nwtis.mbaranasi21.vjezba_05.dnevnik;
+  exports edu.unizg.foi.nwtis.mbaranasi21.vjezba_05.dnevnik.podaci;
+
+  requires java.sql;
+  requires org.apache.commons.csv;
+}
---
+++ mbaranasi21_vjezba_05/mbaranasi21_vjezba_05_lib_konfig/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module mbaranasi21_vjezba_05_lib_konfig {
+  exports edu.unizg.foi.nwtis.konfiguracije;
+
+  requires com.google.gson.Gson;
+}
