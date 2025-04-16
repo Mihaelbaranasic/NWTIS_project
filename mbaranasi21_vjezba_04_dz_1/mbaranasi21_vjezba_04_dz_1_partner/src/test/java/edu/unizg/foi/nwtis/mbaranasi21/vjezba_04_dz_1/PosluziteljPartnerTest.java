e56e6685910219eb081217e4504371ce7f354ba1
Mihael Baranašić <mbaranasi21@foi.hr>
2025-04-16 21:07:44 +0200

    Započeto pisanje testova posluziteljPartner

---
+++ mbaranasi21_vjezba_04_dz_1/mbaranasi21_vjezba_04_dz_1_partner/src/test/java/edu/unizg/foi/nwtis/mbaranasi21/vjezba_04_dz_1/PosluziteljPartnerTest.java
@@ -0,0 +1,28 @@
+package edu.unizg.foi.nwtis.mbaranasi21.vjezba_04_dz_1;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PosluziteljPartnerTest {
+
+  @BeforeAll
+  static void setUpBeforeClass() throws Exception {}
+
+  @AfterAll
+  static void tearDownAfterClass() throws Exception {}
+
+  @BeforeEach
+  void setUp() throws Exception {}
+
+  @AfterEach
+  void tearDown() throws Exception {}
+
+  @Test
+  void testMain() {
+  }
+
+}
