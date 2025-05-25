bfebe9dd9a2aaabc9b388e202ea326cc1b0c3ea0
Mihael Baranašić <mbaranasi21@foi.hr>
2025-05-25 20:05:38 +0200

    Izgradnja kompletnog dokera i testiranje

---
+++ mbaranasi21_vjezba_07_dz_2/mbaranasi21_vjezba_07_dz_2_partner/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module mbaranasi21_vjezba_07_dz_2_partner {
+  exports edu.unizg.foi.nwtis.mbaranasi21.vjezba_07_dz_2;
+
+  requires com.google.gson;
+  requires java.logging;
+  requires mbaranasi21_vjezba_07_dz_2_lib_konfig;
+  requires mbaranasi21_vjezba_07_dz_2_lib_podaci;
+}
